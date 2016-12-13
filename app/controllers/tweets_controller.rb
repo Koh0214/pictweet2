ed23eb98257861dfe3a1e6a14353dbec80ab5bff
岸本 晃平2 <Kohei2@koheimba.local>
2016-12-13 15:46:22 +0900

    コントローラーからnameを入れる場所を削除した。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,7 +10,7 @@ class TweetsController < ApplicationController
   end
 
   def create
-    Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
   def move_to_index
@@ -19,7 +19,7 @@ class TweetsController < ApplicationController
 
   private 
   def tweet_params
-    params.permit(:name, :image, :text)
+    params.permit(:image, :text)
   end
 
 end
