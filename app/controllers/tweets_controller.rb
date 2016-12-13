15aa206b3c9f062c09f567e5ede1c37e2b8fa90e
岸本 晃平2 <Kohei2@koheimba.local>
2016-12-13 12:40:03 +0900

    current_user.idをtweetsの中に入れられるようにした。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-  before_action :move_to_index, except: :index
+    before_action :move_to_index, except: :index 
 
   def index
     @tweets = Tweet.order("id DESC").page(params[:page]).per(5) # Tweet.all.order("id DESC")のallを省略できる。
@@ -10,11 +10,11 @@ class TweetsController < ApplicationController
   end
 
   def create
-    Tweet.create(tweet_params)
+    Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
   def move_to_index
-    redirect_to action: :index
+    redirect_to action: :index unless user_signed_in?
   end
 
   private 
